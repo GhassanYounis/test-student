096fabfda0e192839e1c060e4c88308c11aecac3
Ghassan Younis <ghassanyounis@Ghassans-MacBook-Pro.local>
2023-07-08 19:01:31 +0300

    ver_6

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './style.css';
-import {Router} from "react-router-dom";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <Router basename={process.env.PUBLIC_URL}>< App /></Router>
+  <React>
+    <App />
+  </React>
 );
 
 
